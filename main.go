c66e209c00ca32b788937f5d197e23e1b92fd78f
Tanner Lake <tanner.lake@gmail.com>
2018-02-13 10:27:45 -0800

    Make port configurable from PROXY_PORT env var

--- main.go
+++ main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	socks5 "github.com/armon/go-socks5"
 )
 
 func main() {
-	// Create a SOCKS5 server
 	conf := &socks5.Config{}
 	server, err := socks5.New(conf)
 	if err != nil {
 		panic(err)
 	}
 
-	// Create SOCKS5 proxy on localhost port 2222
-	addr := "0.0.0.0:2222"
+	port := os.Getenv("PROXY_PORT")
+	if port == "" {
+		port = "2222"
+	}
+
+	addr := fmt.Sprintf("0.0.0.0:%s", port)
 	fmt.Println("Starting server on", addr)
 	if err := server.ListenAndServe("tcp", addr); err != nil {
 		panic(err)
